c2afbd49efc25ea72c622d87ffc3d8333373de72
MRAADGH <MrSJGDD@gmail.com>
2024-12-02 02:09:03 +0300

    Update users.php

--- magnusapi/users.php
+++ magnusapi/users.php
@@ -6,7 +6,7 @@ $api_key = 'xcdtvf4567ctfvg56ftvg56ft';
 $api_secret = 'CTFVG567FVTG56FCTVG56FTCVG56ftVG';
 
 $magnusBilling = new MagnusBilling($api_key, $api_secret);
-$magnusBilling->public_url = "http://193.160.130.149/mbilling";
+$magnusBilling->public_url = "http://143.244.139.43/mbilling";
 
 $page = 1;
 
